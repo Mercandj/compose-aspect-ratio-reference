0c75d510f3f1722c4aa15272039d2fa4e89f19a9
Mercandj <modjow.jm9@gmail.com>
2023-04-02 14:49:39 +0200

    [Sample] Remove useless dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,7 +67,6 @@ dependencies {
     implementation("androidx.compose.ui:ui:1.4.0")
     debugImplementation("androidx.compose.ui:ui-tooling:1.4.0")
     implementation("androidx.compose.ui:ui-tooling-preview:1.4.0")
-    implementation("androidx.compose.material:material-icons-extended:1.4.0")
     implementation("androidx.compose.foundation:foundation:1.4.0")
     implementation("androidx.activity:activity-compose:1.7.0")
     implementation("androidx.compose.material3:material3:1.0.1")
